cdc35a6288f5ee455bc5f46174cdd26b1633ebdf
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2026-06-22 21:38:14 +0530

    Update Mavenbuild.groovy

--- demo/vars/Mavenbuild.groovy
+++ demo/vars/Mavenbuild.groovy
@@ -1,6 +1,7 @@
 def call()
 {
 sh 'echo Maven Building the code'
-sh 'mvn clean package -DskipTests'
+sh 'cd java-maven-sonar-argocd-helm-k8s/spring-boot-app && mvn clean package -DskipTests'
+
 
 }
