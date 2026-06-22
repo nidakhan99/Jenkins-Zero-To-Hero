ac419fcd410913a2031ef7cacd9ba11e37d10645
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2026-06-22 21:28:17 +0530

    Create gitcheckout.groovy

---
+++ demo/vars/gitcheckout.groovy
@@ -0,0 +1,6 @@
+def call() {
+
+sh 'echo checking out to the repo with the specified branch'
+git branch: 'main2', credentialsId: 'git_creds', url: 'https://github.com/nidakhan99/Jenkins-Zero-To-Hero.git'
+sh 'Hurray!!!! you have successfully checkout to the repo'
+}
