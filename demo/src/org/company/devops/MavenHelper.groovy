0f01369978d56b56e09a8d1e2c425f33e8d3c7e6
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2026-06-24 11:43:08 +0530

    Update MavenHelper.groovy

--- demo/src/org/company/devops/MavenHelper.groovy
+++ demo/src/org/company/devops/MavenHelper.groovy
@@ -10,16 +10,16 @@ class MavenHelper implements Serializable{
 
     def cleanPackage() {
         script.echo "Running Maven clean package"
-        script.sh "mvn clean package"
+        script.sh "cd java-maven-sonar-argocd-helm-k8s/spring-boot-app && mvn clean package"
     }
 
     def cleanInstall() {
         script.echo "Running Maven clean install"
-        script.sh "mvn clean install"
+        script.sh "cd java-maven-sonar-argocd-helm-k8s/spring-boot-app && mvn clean install"
     }
 
     def test() {
         script.echo "Running Maven tests"
-        script.sh "mvn test"
+        script.sh "cd java-maven-sonar-argocd-helm-k8s/spring-boot-app && mvn test"
     }
 }
