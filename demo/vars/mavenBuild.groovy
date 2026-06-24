57270ccd7410aa2d5b4db965b8cc9f3a287e3ad4
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2026-06-24 11:33:35 +0530

    Update mavenBuild.groovy

--- demo/vars/mavenBuild.groovy
+++ demo/vars/mavenBuild.groovy
@@ -1,7 +1,24 @@
-def call()
-{
-sh 'echo Maven Building the code'
-sh 'cd java-maven-sonar-argocd-helm-k8s/spring-boot-app && mvn clean package -DskipTests'
+//def call()
+//{
+//sh 'echo Maven Building the code'
+//sh 'cd java-maven-sonar-argocd-helm-k8s/spring-boot-app && mvn clean package -DskipTests'
 
 
+//}
+
+
+import org.comapany.devops.MavenHelper
+
+def call(String command = "package") {
+    def maven = new MavenHelper(this)
+
+    if (command == "package") {
+        maven.cleanPackage()
+    } else if (command == "install") {
+        maven.cleanInstall()
+    } else if (command == "test") {
+        maven.test()
+    } else {
+        error "Unsupported Maven command: ${command}"
+    }
 }
