76a60ee1db64e736539a543985deac294673ae23
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2026-06-24 13:02:59 +0530

    Update sonarScan.groovy

--- demo/vars/sonarScan.groovy
+++ demo/vars/sonarScan.groovy
@@ -1,12 +1,12 @@
 import org.company.devops.SonarHelper
 
-
 def call(Map config = [:]) {
-    String sonarUrl = config.sonarUrl
+    String projectDir = config.projectDir ?: error("projectDir is required")
+    String sonarUrl = config.sonarUrl ?: error("sonarUrl is required")
     String credentialsId = config.credentialsId ?: 'sonarqube'
 
     withCredentials([string(credentialsId: credentialsId, variable: 'SONAR_AUTH_TOKEN')]) {
-        def scanner = new sonarScan(this)
-        scanner.run(sonarUrl)
+        def sonar = new sonarScan(this)
+        sonar.scan(projectDir, sonarUrl)
     }
 }
