b2067d0453363f6d198b7a71c35c70bedef887fc
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2026-06-24 15:02:53 +0530

    Update dockerBuild.groovy

--- demo/vars/dockerBuild.groovy
+++ demo/vars/dockerBuild.groovy
@@ -1,13 +1,10 @@
 import org.company.devops.DockerHelper
 
 def call(Map config = [:]) {
-
     String projectDir = config.projectDir ?: error("projectDir is required")
     String dockerImage = config.dockerImage ?: error("dockerImage is required")
     String credentialsId = config.credentialsId ?: 'docker-cred'
 
-    { def dockerHelper = new DockerHelper(this)
-
+    def dockerHelper = new DockerHelper(this)
     dockerHelper.buildAndPush(projectDir, dockerImage, credentialsId)
-    }
 }
