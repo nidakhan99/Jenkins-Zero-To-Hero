31ec5475811f8453b69bd88020149ee0908ee882
nidakhan99 <153353850+nidakhan99@users.noreply.github.com>
2026-06-24 14:07:46 +0530

    Create dockerBuild.groovy

---
+++ demo/vars/dockerBuild.groovy
@@ -0,0 +1,13 @@
+import org.company.devops.DockerHelper
+
+def call(Map config = [:]) {
+    String projectDir = config.projectDir ?: error("projectDir is required")
+    String DOCKER_IMAGE = config.sonarUrl ?: error("DockerImage is required")
+    String REGISTRY_CREDENTIALS = config.REGISTRY_CREDENTIALS ?: 'docker-cred'
+
+    {
+        def docker = new DockerHelper(this)
+        docker.build(projectDir, DOCKER_IMAGE, docker-cred)
+    }
+}
+
